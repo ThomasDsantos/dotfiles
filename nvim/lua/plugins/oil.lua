75904450d82a143f9c904f4bc0234dd0d8b38f93
Thomas Dos Santos <thomas.dossantos@numberly.com>
2025-10-01 11:21:40 +0200

    feat(oil): add git ignored hide keymap, customize bar

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,51 +1,123 @@
+-- helper function to parse output
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+local git_status = new_git_status()
+
+
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 return {
-  -- "nvim-neo-tree/neo-tree.nvim",
-  -- dependencies = {
-  --   "nvim-lua/plenary.nvim",
-  --   "nvim-tree/nvim-web-devicons",
-  --   "MunifTanjim/nui.nvim",
-  -- },
-  -- config = function()
-  --   require('neo-tree').setup({
-  --     close_if_last_window = true,
-  --     filesystem = {
-  --       filtered_items = {
-  --         hide_dotfiles = false,
-  --         hide_gitignored = true,
-  --         hide_by_name = {
-  --           ".git"
-  --         },
-  --       },
-  --       window = {
-  --         mappings = {
-  --           ["B"] = "noop"
-  --         },
-  --       },
-  --       follow_current_file = {
-  --         enabled = true
-  --       },
-  --     },
-  --     event_handlers = {
-  --       {
-  --         event = "neo_tree_buffer_enter",
-  --         handler = function()
-  --           vim.cmd [[
-  --           setlocal relativenumber
-  --         ]]
-  --         end,
-  --       }
-  --     },
-  --   })
-  --   vim.keymap.set('n', '<leader>b', '<Cmd>Neotree toggle<CR>')
-  -- end,
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
+  opts = function()
+    local detail = false
+    local git_filter_enabled = true
+
+    local function is_hidden_file(name, bufnr)
+      if not git_filter_enabled then
+        return false -- Do not hide anything if filter is disabled
+      end
+      local dir = require("oil").get_current_dir(bufnr)
+      local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+      if not dir then
+        return is_dotfile
+      end
+      if is_dotfile then
+        return not git_status[dir].tracked[name]
+      else
+        return git_status[dir].ignored[name]
+      end
+    end
+
+    return {
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+        ["gh"] = {
+          desc = "Toggle git file filter",
+          callback = function()
+            git_filter_enabled = not git_filter_enabled
+            require("oil").set_is_hidden_file(is_hidden_file)
+          end,
+        },
+      },
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          local dir = require("oil").get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then
+            return is_dotfile
+          end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
+      },
+    }
+  end,
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   keys = {
     { "<leader>b", "<cmd>Oil<cr>" },
